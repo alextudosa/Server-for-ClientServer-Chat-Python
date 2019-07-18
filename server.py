a95c0964628d786e281e582c67955718e6ab9356
alextudosa <tudosa_alex@yahoo.com>
2019-07-18 16:32:57 +0300

    Communication between two or more clients

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ while True:
     connection, server_addr = sock.accept()
     try:
         while True:
+            clientIp = connection.getpeername()[0]
 
             print('Connected: ', connection)
             data = connection.recv(1024)
@@ -43,8 +44,8 @@ while True:
                     #case client sent message
                     print('mesaj', 'nu Connect')
                     file3 = open(pathToFile, "a+")
-                    file3.write("Client: " + data.decode('utf-8') + "\n")
-                    file3.write("Server: " + data.decode('utf-8') + "\n")
+                    messageToWriteInFile = "Client-" + clientIp + ": "
+                    file3.write(messageToWriteInFile + data.decode('utf-8') + "\n")
                     file3.close()
                 file2 = open(pathToFile, "r+")
                 f2 = file2.readlines()
