bf26c82ed5a92b32c834fcf9aa519556daa7253e
alextudosa <tudosa_alex@yahoo.com>
2019-07-22 17:16:12 +0300

    Connecting to server using username and password verification

--- server.py
+++ server.py
@@ -8,6 +8,13 @@ server_addr = ('0.0.0.0', 8080)
 sock.bind(server_addr)
 sock.listen(1)
 
+
+user1 = 'alex'
+password1 = 'pass1'
+user2 = 'vali'
+password2 = 'pass2'
+goodCredentialsOrNot = 0
+
 dirName = '/home/alex/PycharmProjects/chatDir'
 fileName = 'chatConversation.txt'
 pathToFile = dirName + '/' + fileName
@@ -30,30 +37,42 @@ while True:
             data = connection.recv(1024)
             print(data.decode('utf-8'))
             if data:
-                if data.decode('utf-8') == "#$":
+
+                myData = data.decode()
+                c = myData.split(", ")
+                if c.__contains__(user1) and c.__contains__(password1):
+                    userConnected = user1
+                elif c.__contains__(user2) and c.__contains__(password2):
+                    userConnected = user2
+                else:
+                    userConnected = 'NoAccount'
+
+
+                if myData.split(", ")[2] == "#$" and userConnected != 'NoAccount':
                     # case client just want to connect
-                    print('Mmodal', "Momsa")
+
                     file2 = open(pathToFile, "r+")
                     f2 = file2.readlines()
                     for y in f2:
                         print(y)
                         connection.send(y.encode())
                     file2.close()
-                    connection.send("#$\n".encode())
-                else:
+                    connection.send((userConnected + '\n').encode())
+                elif userConnected != 'NoAccount' and myData.split(", ")[2] != "#$":
                     #case client sent message
-                    print('mesaj', 'nu Connect')
                     file3 = open(pathToFile, "a+")
-                    messageToWriteInFile = "Client-" + clientIp + ": "
-                    file3.write(messageToWriteInFile + data.decode('utf-8') + "\n")
+                    messageToWriteInFile = "Client-" + userConnected + ": " + myData.split(", ")[2]
+                    file3.write(messageToWriteInFile + "\n")
                     file3.close()
-                file2 = open(pathToFile, "r+")
-                f2 = file2.readlines()
-                for y in f2:
-                    print(y)
-                    connection.send(y.encode())
-                file2.close()
+                    file2 = open(pathToFile, "r+")
+                    f2 = file2.readlines()
+                    for y in f2:
+                        print(y)
+                        connection.send(y.encode())
+                    file2.close()
+                elif userConnected == 'NoAccount':
 
+                    connection.send((userConnected + "\n").encode())
             elif not data:
                 break
 
