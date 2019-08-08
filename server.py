d9f928db583c21aa42e84c2817cdd659965a5632
alextudosa <tudosa_alex@yahoo.com>
2019-08-08 13:32:17 +0300

    Connection with SSL certificate security

--- server.py
+++ server.py
@@ -1,12 +1,36 @@
 import socket
+from socket import AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET, SHUT_RDWR
 import sys
 import os
+import ssl
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# server_addr = ('0.0.0.0', 8080)
+# sock.bind(server_addr)
+# sock.listen(1)
 
-server_addr = ('0.0.0.0', 8080)
-sock.bind(server_addr)
-sock.listen(1)
+listen_addr = '172.18.81.40'
+listen_port = 8080
+server_cert = 'server.crt'
+server_key = 'server.key'
+client_certs = 'client.crt'
+PYTHONHTTPSVERIFY = 0
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.options &= ssl.PROTOCOL_TLS_SERVER
+context.options &= ssl.OP_NO_SSLv2
+context.options &= ssl.OP_NO_SSLv3
+#context.verify_mode = ssl.CERT_REQUIRED
+
+context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+
+context.load_verify_locations(cafile=client_certs)
+
+
+bindsocket = socket.socket()
+bindsocket.bind((listen_addr, listen_port))
+bindsocket.listen(5)
 
 
 
@@ -30,13 +54,15 @@ file2 = open(pathToFile, "r+")
 
 
 while True:
-    connection, server_addr = sock.accept()
+    newsocket, fromaddr = bindsocket.accept()
     try:
+        conn = context.wrap_socket(newsocket, server_side=True, do_handshake_on_connect=False)
+        conn.do_handshake()
         while True:
-            clientIp = connection.getpeername()[0]
-            print('Connected: ', connection)
+            clientIp = conn.getpeername()[0]
+            print('Connected: ', conn)
             print('IP: ', clientIp)
-            data = connection.recv(1024)
+            data = conn.recv(1024)
             # print(data.decode('utf-8'))
             if data:
 
@@ -62,9 +88,9 @@ while True:
                     f2 = file2.readlines()
                     for y in f2:
                         print(y)
-                        connection.send(y.encode())
+                        conn.send(y.encode())
                     file2.close()
-                    connection.send((userConnected + '\n').encode())
+                    conn.send((userConnected + '\n').encode())
                 elif userConnected != 'NoAccount' and myData.split(", ")[2] != "#$":
                     #case client sent message
                     file3 = open(pathToFile, "a+")
@@ -75,15 +101,16 @@ while True:
                     f2 = file2.readlines()
                     for y in f2:
                         print(y)
-                        connection.send(y.encode())
+                        conn.send(y.encode())
                     file2.close()
                 elif userConnected == 'NoAccount':
 
-                    connection.send((userConnected + "\n").encode())
+                    conn.send((userConnected + "\n").encode())
             elif not data:
                 break
-
     finally:
-        connection.close()
+        print("Closing connection")
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
 
 
